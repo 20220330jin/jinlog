670ca08ed9bc5c80dd28c520d6732c1b08c6299f
hjkim <20220330@gmail.com>
2024-08-21 18:34:28 +0900

    [수정] 배포에러 수정될듯?

--- jinlog/next.config.js
+++ jinlog/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'react-quill$': 'react-quill/dist/react-quill.js',
+        }
+        return config
+    },
 }
 
 module.exports = nextConfig
